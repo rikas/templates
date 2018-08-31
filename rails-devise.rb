cce2ad8470ca6d6e0d877add84e2ce9e5658880b
Ricardo Otero <oterosantos@gmail.com>
2018-08-31 15:57:09 +0100

    Remove teste directory

--- rails-devise.rb
+++ rails-devise.rb
@@ -54,12 +54,14 @@ file 'Gemfile', <<~GEMFILE_CONTENT
     gem 'listen', '~> 3.0.5'
     gem 'pry-byebug'
     gem 'pry-rails'
+    gem 'spring', require: false
   end
 GEMFILE_CONTENT
 
 ########################################
 # Ruby version
 ########################################
+run '.ruby-version'
 file '.ruby-version', RUBY_VERSION
 
 ########################################
@@ -86,6 +88,91 @@ file 'app/views/shared/_flashes.html.erb', <<~HTML
     </div>
   <% end %>
 HTML
+
+file '.rspec', <<~RSPEC
+  --color
+  --format documentation
+  --order random
+  --require rails_helper
+RSPEC
+
+file 'spec/rails_helper.rb', <<~HELPER
+  # frozen_string_literal: true
+
+  ENV['RAILS_ENV'] ||= 'test'
+
+  # Configure SimpleCov
+  require 'simplecov'
+  SimpleCov.start do
+    load_profile 'test_frameworks'
+
+    add_filter %r{^/config/}
+    add_filter %r{^/db/}
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Policies', 'app/policies'
+    add_group 'Services', 'app/services'
+    add_group 'Jobs', 'app/jobs'
+    add_group 'Decorators', 'app/decorators'
+    add_group 'Libraries', 'lib/'
+
+    track_files '{app,lib}/**/*.rb'
+  end
+
+  require File.expand_path('../config/environment', __dir__)
+
+  # Prevent database truncation if the environment is production
+  abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+  require 'rspec/rails'
+  require 'factory_bot'
+  require 'database_cleaner'
+  require 'vcr'
+  require 'webmock/rspec'
+
+  ActiveRecord::Migration.maintain_test_schema!
+
+  WebMock.disable_net_connect!
+
+  VCR.configure do |config|
+    config.cassette_library_dir = 'spec/cassettes'
+    config.hook_into :webmock
+    config.ignore_localhost = true
+  end
+
+  RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include Warden::Test::Helpers
+
+    # Include FactoryBot so we can use 'create' instead of 'create'
+    config.include FactoryBot::Syntax::Methods
+
+    config.use_transactional_fixtures = false
+
+    config.infer_spec_type_from_file_location!
+
+    # Filter lines from Rails gems in backtraces.
+    config.filter_rails_from_backtrace!
+
+    # Arbitrary gems may also be filtered via:
+    # config.filter_gems_from_backtrace("gem name")
+
+    config.before(:all) do
+      DatabaseCleaner.strategy = :truncation, { except: %w[spatial_ref_sys] }
+      DatabaseCleaner.start
+    end
+
+    config.after(:all) do
+      DatabaseCleaner.clean
+    end
+  end
+HELPER
+
+run 'touch .env'
+run 'curl -L https://raw.githubusercontent.com/rikas/templates/master/.rubocop.yml > .rubocop.yml'
 
 ########################################
 # Generators
@@ -181,93 +268,7 @@ after_bundle do
     JS
   end
 
-  run 'touch .env'
-
-  run 'curl -L https://raw.githubusercontent.com/rikas/templates/master/.rubocop.yml > .rubocop.yml'
-
   git :init
   git add: '.'
   git commit: "-m 'Initial commit'"
 end
-
-file '.rspec', <<~RSPEC
-  --color
-  --format documentation
-  --order random
-  --require rails_helper
-RSPEC
-
-file 'spec/rails_helper.rb', <<~HELPER
-  # frozen_string_literal: true
-
-  ENV['RAILS_ENV'] ||= 'test'
-
-  # Configure SimpleCov
-  require 'simplecov'
-  SimpleCov.start do
-    load_profile 'test_frameworks'
-
-    add_filter %r{^/config/}
-    add_filter %r{^/db/}
-
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Models', 'app/models'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Policies', 'app/policies'
-    add_group 'Services', 'app/services'
-    add_group 'Jobs', 'app/jobs'
-    add_group 'Decorators', 'app/decorators'
-    add_group 'Libraries', 'lib/'
-
-    track_files '{app,lib}/**/*.rb'
-  end
-
-  require File.expand_path('../config/environment', __dir__)
-
-  # Prevent database truncation if the environment is production
-  abort('The Rails environment is running in production mode!') if Rails.env.production?
-
-  require 'rspec/rails'
-  require 'factory_bot'
-  require 'database_cleaner'
-  require 'vcr'
-  require 'webmock/rspec'
-
-  ActiveRecord::Migration.maintain_test_schema!
-
-  WebMock.disable_net_connect!
-
-  VCR.configure do |config|
-    config.cassette_library_dir = 'spec/cassettes'
-    config.hook_into :webmock
-    config.ignore_localhost = true
-  end
-
-  RSpec.configure do |config|
-    config.include Devise::Test::ControllerHelpers, type: :controller
-    config.include Warden::Test::Helpers
-
-    # Include FactoryBot so we can use 'create' instead of 'create'
-    config.include FactoryBot::Syntax::Methods
-
-    config.use_transactional_fixtures = false
-
-    config.infer_spec_type_from_file_location!
-
-    # Filter lines from Rails gems in backtraces.
-    config.filter_rails_from_backtrace!
-
-    # Arbitrary gems may also be filtered via:
-    # config.filter_gems_from_backtrace("gem name")
-
-    config.before(:all) do
-      DatabaseCleaner.strategy = :truncation, { except: %w[spatial_ref_sys] }
-      DatabaseCleaner.start
-    end
-
-    config.after(:all) do
-      DatabaseCleaner.clean
-    end
-  end
-HELPER
